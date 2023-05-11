d64c9534d10fcfc40c420ba9912ef083ea77c889
Nguyen Khanh <87373594+nguyenkhanh2kpi@users.noreply.github.com>
2023-05-11 14:45:58 +0700

    setting

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -10,6 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+## tự code
+import os
+
+# set the idle timeout to 10 days (in seconds)
+IDLE_TIMEOUT = 864000
+
+# get the DATABASE_URL from environment variables
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
